91e6d2c6fcef17a94a751f025b146e4c35e584fc
Nick Jennings <nije@microsoft.com>
2025-09-27 13:07:30 -0700

    Initial

---
+++ Shopify/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Shopify
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalPart1 = 0;
+            var totalPart2 = 0;
+
+            Console.WriteLine($"Part 1: {totalPart1}");
+            Console.WriteLine($"Part 2: {totalPart2}");
+        }
+    }
+}
